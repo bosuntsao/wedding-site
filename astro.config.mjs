90666cf0e2c4313a3032a377098279c70dda11c8
bosuntsao <tsao031810@gmail.com>
2025-04-22 21:46:37 +0800

    修正 deploy.yml 並準備部署

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,6 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-// import mdx from '@astrojs/mdx';
-// import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
-	// site: 'https://example.com',
-	// integrations: [mdx(), sitemap()],
-	site: 'https://bosuntsao.github.io',
+  site: 'https://bosuntsao.github.io',
   base: '/wedding-site',
 });
