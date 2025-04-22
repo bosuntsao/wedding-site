329e82e70a4641d885be215b96f00c32b2474d1c
bosuntsao <tsao031810@gmail.com>
2025-04-22 21:10:09 +0800

    將專案移至根目錄並準備部署

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+// import mdx from '@astrojs/mdx';
+// import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
