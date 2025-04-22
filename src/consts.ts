92e008cc95186f7aebdb032cc83299ea23783465
bosuntsao <tsao031810@gmail.com>
2025-04-22 22:49:56 +0800

    修正 deploy.yml 並準備部署

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
+// src/consts.ts
 
-// export const SITE_TITLE = 'Astro Blog';
-// export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Our Wedding site";
+export const SITE_DESCRIPTION = "Our happy story starts here";
+
